ae897c76133b8d3add83e4a6b4ea61f90af89017
mephisto <mephisto2120@gmail.com>
2021-03-27 19:14:01 +0100

    Modifying Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,52 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 import './HeaderStyle.css';
 
-class Header extends Component {
-    // renderLinks() {
-    //     if (this.props.authenticated) {
-    //         return (
-    //             <div>
-    //                 <Link to="/signout">Sign Out</Link>
-    //         <Link to="/feature">Feature</Link>
-    //             </div>
-    //     );
-    //     } else {
-    //         return (
-    //             <div>
-    //                 <Link to="/signup">Sign Up</Link>
-    //         <Link to="/signin">Sign In</Link>
-    //         </div>
-    //     );
-    //     }
-    // }
+type AuthProps = {
+  authenticated: string;
+};
 
-    render() {
-        return (
-            <div className="header">
-            <Link to="/">Redux Auth</Link>
-        {/*{this.renderLinks()}*/}
+type Auth = {
+  authenticated: string;
+  errorMessage: string;
+};
+
+type AuthState = {
+  auth: Auth;
+};
+
+class Header extends Component<AuthProps, AuthState> {
+  renderLinks() {
+    if (this.props.authenticated) {
+      return (
+        <div>
+          <Link to="/signout">Sign Out</Link>
+          <Link to="/feature">Feature</Link>
         </div>
-    );
+      );
+    } else {
+      return (
+        <div>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/signin">Sign In</Link>
+        </div>
+      );
     }
+  }
+
+  render() {
+    return (
+      <div className="header">
+        <Link to="/">Redux Auth</Link>
+        {this.renderLinks()}
+      </div>
+    );
+  }
 }
 
-function mapStateToProps(state : any) {
-    return { authenticated: state.auth.authenticated };
+function mapStateToProps(state: AuthState) {
+  return {authenticated: state.auth.authenticated};
 }
 
 export default connect(mapStateToProps)(Header);
