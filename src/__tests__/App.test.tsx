2122407c55070cc6fba7e677091ad255429a975f
mephisto2120 <mephisto2120@gmail.com>
2021-03-30 12:41:08 +0200

    Scaffolding Signup form

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import {shallow} from 'enzyme';
+import App from 'App';
+import Header from 'components/Header';
 
-it('does nothing', () => {
+it('should contain Header', () => {
+  const wrapped = shallow(<App/>);
+
+  expect(wrapped.find(Header).length).toEqual(1);
 });
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Welcome from 'components/Welcome';
+import Signup from 'components/Signup';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App>
         <Route path="/" exact component={Welcome} />
+        <Route path="/" component={Signup} />
       </App>
     </BrowserRouter>
   </React.StrictMode>,
