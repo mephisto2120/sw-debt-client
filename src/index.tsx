ca479c9a29872904bbf735660f04e7e612e1877e
mephisto2120 <mephisto2120@gmail.com>
2021-03-30 21:24:39 +0200

    Including redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
 import { BrowserRouter, Route } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+import App from 'App';
 import Welcome from 'components/Welcome';
 import Signup from 'components/Signup';
 
+const store = createStore(
+  reducers,
+  {}
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App>
-        <Route path="/" exact component={Welcome} />
-        <Route path="/" component={Signup} />
-      </App>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App>
+          <Route path="/" exact component={Welcome}/>
+          <Route path="/" component={Signup}/>
+        </App>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.querySelector('#root')
 );
