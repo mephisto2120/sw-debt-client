15b31b5a436bc05ba2d22fde3c6db9d3bd70830c
mephisto2120 <mephisto2120@gmail.com>
2021-03-30 21:33:51 +0200

    ReduxForm for Signup

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { reduxForm, Field } from 'redux-form';
 
 class Signup extends Component {
   render() {
@@ -6,9 +7,21 @@ class Signup extends Component {
       <form>
         <fieldset>
           <label>Email</label>
+          <Field
+            name="email"
+            type="text"
+            component="input"
+            autoComplete="none"
+          />
         </fieldset>
         <fieldset>
           <label>Password</label>
+          <Field
+            name="password"
+            type="password"
+            component="input"
+            autoComplete="none"
+          />
         </fieldset>
         <button>Sign Up!</button>
       </form>
@@ -16,4 +29,5 @@ class Signup extends Component {
   }
 }
 
-export default Signup;
+// @ts-ignore
+export default reduxForm({ form: 'signup' })(Signup);
